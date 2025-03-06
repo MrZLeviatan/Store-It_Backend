5fd5ae27ba72afb2793594239d56c4838b69cf17
Nicolas C.S <nikis281002@gmail.com>
2025-03-06 00:40:14 -0500

    Configuración Inicial...
    Se despliega la configuración inicial para el desarrollo de la app con la estructura de carpetas y propiedades para la conexión a una base de datos SQL Oracle y el framework Spring Boot.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,41 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "co.edu.uniquindio"
 version = "1.0-SNAPSHOT"
+description =  "Firma ROT5:  QwE Pjanfyfr(Nicolas Cabrera Serrano)"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+        compileOnly {
+            extendsFrom(configurations.annotationProcessor.get())
+        }
+    }
 
 repositories {
     mavenCentral()
 }
 
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.hibernate:hibernate-core:6.4.0.Final")
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+    implementation("com.sun.mail:javax.mail:1.6.2") // Java Mail Sender
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("com.oracle.database.jdbc:ojdbc8:21.9.0.0") // Oracle SQL
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
---
+++ src/main/java/co/edu/uniquindio/project/StoreItApplication.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // Indica que es la clase principal de Spring Boot
+public class StoreItApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StoreItApplication.class, args);  // Inicia la aplicación
+    }
+}
