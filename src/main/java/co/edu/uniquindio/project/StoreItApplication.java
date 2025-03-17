e789955728e6595f282ba2e585bc65b689753f9e
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2025-03-17 02:30:46 -0500

    agregacion etiqueta
    agrege la etiqueta @ComponentScan para decirle a spring boot donde van a estar los controladores

--- src/main/java/co/edu/uniquindio/project/StoreItApplication.java
+++ src/main/java/co/edu/uniquindio/project/StoreItApplication.java
@@ -2,8 +2,10 @@ package co.edu.uniquindio.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication  // Indica que es la clase principal de Spring Boot
+@ComponentScan(basePackages = "co.edu.uniquindio.controller")
 public class StoreItApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoreItApplication.class, args);  // Inicia la aplicación
