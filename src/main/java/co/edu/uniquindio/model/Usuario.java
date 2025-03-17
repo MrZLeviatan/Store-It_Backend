f6610329f11b79f21b1e4211473ba5396bdfe82c
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2025-03-17 02:31:11 -0500

    Model
    clase para representar a los usuarios de nuestra app

---
+++ src/main/java/co/edu/uniquindio/model/Usuario.java
@@ -0,0 +1,19 @@
+package co.edu.uniquindio.model;
+
+import co.edu.uniquindio.model.enums.Rol;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Usuario {
+
+    private String id,gmail,passaword,cedula,nombre;
+    private Rol rol;
+
+}
