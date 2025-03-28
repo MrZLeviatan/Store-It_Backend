0853eb832494a32f80f48c90d7887b34b3835adb
Nicolas C.S <nikis281002@gmail.com>
2025-03-28 16:21:08 -0500

    Test Crud
    Se crea y Testea el crud de la gestion de clientes

--- src/test/java/co/edu/uniquindio/controller/ClienteControlerTest.java
+++ src/test/java/co/edu/uniquindio/controller/ClienteControlerTest.java
@@ -14,8 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = StoreItApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -34,6 +33,26 @@ public class ClienteControlerTest {
 
         //Se crea un objeto para realizar la creación de la cuenta
         CrearClienteDTO cuentaDTO = new CrearClienteDTO(
+                "102",
+                "Luis Perez",
+                "luis@example.com",
+                "1234567890"
+        );
+
+        //Se realiza la petición POST al servidor usando el MockMvc y se valida que el estado de la respuesta sea 201
+        mockMvc.perform(post("/api/clientes")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(cuentaDTO)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isCreated()); // Espera que la respuesta tenga el código 201 (CREATED)
+
+     }
+
+    @Test
+    public void crearIncorrectoTest() throws Exception {
+
+        //Se crea un objeto para realizar la creación de la cuenta
+        CrearClienteDTO cuentaDTO = new CrearClienteDTO(
                 "1001330212",
                 "Luis Perez",
                 "nicolas@example.com",
@@ -45,9 +64,8 @@ public class ClienteControlerTest {
                         .contentType("application/json")
                         .content(objectMapper.writeValueAsString(cuentaDTO)))
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isCreated()); // Espera que la respuesta tenga el código 201 (CREATED)
-
-     }
+                .andExpect(status().isBadRequest());
+    }
 
     @Test
     public void actualizarTest() throws Exception {
@@ -68,4 +86,35 @@ public class ClienteControlerTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isOk());
     }
+
+    @Test
+    public void eliminarTest() throws Exception {
+
+        // Simulación de la cookie de sesión con el nombre SESSIONID
+        MockHttpServletRequestBuilder request = delete("/api/clientes/{id}", "102")
+                .cookie(new Cookie("SESSIONID", "valor_de_sesion")) // Aquí debes colocar el valor real de la cookie
+                .contentType("application/json");
+
+        // Realiza la petición y valída que el estado dé la respuesta sea OK
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+    }
+
+
+    @Test
+    public void obtenerTest() throws Exception {
+
+        // Simulación de la cookie de sesión con el nombre SESSIONID
+        MockHttpServletRequestBuilder request = get("/api/clientes/{id}", "1001330212")
+                .cookie(new Cookie("SESSIONID", "valor_de_sesion")) // Reemplaza "valor_de_sesion" con un valor válido
+                .contentType("application/json");
+
+        // Realiza la petición y valída que el estado dé la respuesta sea OK
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+    }
+
+
 }
