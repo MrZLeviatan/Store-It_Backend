76e371c6c0b27247374974bd75a82e45708f838b
David Batero <davidbatero.r@gmail.com>
2025-04-03 06:49:43 -0500

    Avance

--- src/main/java/co/edu/uniquindio/mapper/PersonalBodegaMapper.java
+++ src/main/java/co/edu/uniquindio/mapper/PersonalBodegaMapper.java
@@ -1,38 +1,11 @@
+
 package co.edu.uniquindio.mapper;
 
 import co.edu.uniquindio.dto.PersonalBodegaDTO;
 import co.edu.uniquindio.model.*;
-//Manual, se puede hacer autmaticametne con un MapStructud
-public class PersonalBodegaMapper implements PersonalBodegaMapperImp {
-    @Override
-    public PersonalBodega toEntity(PersonalBodegaDTO pbDto) {
-        if (pbDto == null) {
-            return null;
-        }
-        return new PersonalBodega(
-                pbDto.id(),
-                pbDto.nombre(),
-                pbDto.apellido(),
-                pbDto.email(),
-                pbDto.telefono(),
-                pbDto.cargo(),
-                pbDto.fechaIngreso()
-        );
-    }
 
-    @Override
-    public PersonalBodegaDTO toDTO(PersonalBodega pb) {
-        if (pb == null) {
-            return null;
-        }
-        return new PersonalBodegaDTO(
-                pb.getId(),
-                pb.getNombre(),
-                pb.getApellido(),
-                pb.getEmail(),
-                pb.getTelefono(),
-                pb.getCargo(),
-                pb.getFechaIngreso()
-        );
-    }
+
+public interface PersonalBodegaMapperImp {
+    PersonalBodega toEntity(PersonalBodegaDTO pbDto);
+    PersonalBodegaDTO toDTO(PersonalBodega pb);
 }
